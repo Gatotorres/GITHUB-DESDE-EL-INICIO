9f0cf49b6131cf150e0a35ef93dc01fbb7bc47fe
Gatotorres <sr.nahueltorres@gmail.com>
2023-03-30 21:17:46 +0200

    clase 8 agregada al index, toggle burguer menu funcionando con js

--- MAIN.js
+++ MAIN.js
@@ -6,3 +6,12 @@ NavMenuEmail.addEventListener('click', toggleDesktopMenu);
 function toggleDesktopMenu () {
     DesktopMenu.classList.toggle('inactive');
 }
+
+const IconBurguerMenu  = document.querySelector('.icon-burguer-menu');
+const BurguerMenuMobile  = document.querySelector('.burguer-menu-mobile');
+
+IconBurguerMenu.addEventListener('click', toggleBurguerMenu);
+
+function toggleBurguerMenu () {
+    BurguerMenuMobile.classList.toggle('inactive');
+}
